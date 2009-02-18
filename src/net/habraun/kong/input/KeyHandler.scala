556e5fb7cc740f407b5d2799a321c6b689326dad
Hanno Braun <hanno@habraun.net>
2009-02-18 14:24:04 +0100

    Input: Updated KeyHandler documentation.

--- src/net/habraun/kong/input/KeyHandler.scala
+++ src/net/habraun/kong/input/KeyHandler.scala
@@ -27,15 +27,15 @@ import edu.umd.cs.piccolo.event._
 
 
 /**
- * Handles keyboard input events from the Piccolo2D scene graph and provides a method, that allows you to
+ * Handles keyboard input events from the Piccolo2D scene graph and provides methods, that allow you to
  * easily determine which keys are currently pressed. The intended usage is, to set Piccolo's keyboard focus
  * to this class and then query it every frame from the main loop.
  *
  * Example setup:
  * // Configure a KeyMap. Check KeyMap's documentation for information on how to do this.
- * val inputHandler = new InputHandler(keyMap)
+ * val keyHandler = new KeyHandler(keyMap)
  * val canvas = PCanvas
- * canvas.getRoot.getDefaultInputManager.setKeyboardFocus(inputHandler)
+ * canvas.getRoot.getDefaultInputManager.setKeyboardFocus(keyHandler)
  *
  * By default you'll have to click into the canvas before keyboard events will be delivered to the handler.
  * If you'd like to deliver keyboard events by default right after the application has been started, you'll
@@ -45,16 +45,24 @@ import edu.umd.cs.piccolo.event._
  *
  * Example usage:
  * while (true) {
- *     // Tell the input handler to execute an action if a certain player has pressed a certain key. Use the
- *     // player and key objects you defined when configuring the KeyMap.
- *     doIfPressed(player, key, () => {
+ *     // Tell the input handler to execute an action if a specific player has pressed a specific key. Use
+ *     // the player and key objects you defined when configuring the KeyMap.
+ *     keyHandler.doIfPressed(player, key, () => {
  *         // do stuff here
  *     })
  *
  *     // Also works if your stuff returns something.
- *     val result = doIfPressed(player, key, () => {
+ *     val result = keyHandler.doIfPressed(player, key, () => {
  *         // do stuff that returns a result; your result will be wrapped in an Option
  *     })
+ *
+ *     // If you'd like to know if the player pressed one of several keys, do something like this.
+ *     if (keyHandler.isPressed(player, someKey)
+ *         // do stuff
+ *     else if (keyHandler.isPressed(player, someOtherKey)
+ *         // do other stuff
+ *     else
+ *         // do something else
  *
  *     // Do whatever else you need to do in your main loop
  * }
@@ -94,6 +102,8 @@ class KeyHandler(keyMap: KeyMap) {
 
 	/**
 	 * Executes an action if a specific player has pressed a specific key.
+	 * Returns Some(result) if the key is pressed, result being the result returned by your action. Returns
+	 * None if the key is not pressed.
 	 */
 
 	def doIfPressed[R](player: Player, key: Key, action: () => R): Option[R] = {
