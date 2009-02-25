8a4ad04c739f6166ab4e4ff17c762c73e057cc99
Hanno Braun <hanno@habraun.net>
2009-02-25 15:52:47 +0100

    Physics: Added a simple world class that can apply forces to bodies and move them.

---
+++ src/net/habraun/kong/physics/World.scala
@@ -0,0 +1,73 @@
+/*
+	Copyright (c) 2009 Hanno Braun <hanno@habraun.net>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+
+package net.habraun.kong.physics
+
+
+
+import scala.collection.mutable._
+
+
+
+/**
+ * The central class for the physics simulation.
+ * World is basically a container for objects, whose attributes it updates every simulation step.
+ */
+
+class World {
+
+	private[this] var bodies = new HashSet[Body]
+
+
+
+	/**
+	 * Adds a body to the world. The body will be simulated until it is removed.
+	 */
+	
+	def add(body: Body) {
+		bodies.addEntry(body)
+	}
+
+
+
+	/**
+	 * Removes the body from the world. The body will no longer be simulated.
+	 */
+
+	def remove(body: Body) {
+		bodies.removeEntry(body)
+	}
+	
+
+
+	/**
+	 * Steps the physics simulation.
+	 * All bodies are moved, according to their velocity and the forces that are applied to them.
+	 */
+	
+	def step(delta: Double) {
+		bodies.foreach((body) => {
+			// Apply forces.
+			body.velocity = body.velocity + body.appliedForce / body.mass * delta
+			body.resetForce
+
+			// Move bodies.
+			body.position = body.position + (body.velocity * delta)
+		})
+	}
+}
---
+++ test/net/habraun/kong/physics/WorldTest.scala
@@ -0,0 +1,79 @@
+/*
+	Copyright (c) 2009 Hanno Braun <hanno@habraun.net>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+
+package net.habraun.kong.physics
+
+
+
+import org.junit._
+import org.junit.Assert._
+
+
+
+class WorldTest {
+
+	@Test
+	def addBodyStepExpectBodyMoved {
+		val world = new World
+		val body = new Body
+		body.velocity = Vec2D(1, 0)
+		world.add(body)
+		world.step(2.0)
+		assertEquals(Vec2D(2, 0), body.position)
+	}
+
+
+
+	@Test
+	def addAndRemoveBodyExpectBodyNotMoved {
+		val world = new World
+		val body = new Body
+		body.velocity = Vec2D(1, 0)
+		world.add(body)
+		world.remove(body)
+		world.step(2.0)
+		assertEquals(Vec2D(0, 0), body.position)
+	}
+
+
+
+	@Test
+	def addBodyApplyForceCheckVelocity {
+		val world = new World
+		val body = new Body
+		body.mass = 5
+		body.applyForce(Vec2D(5, 0))
+		world.add(body)
+		world.step(2.0)
+		assertEquals(Vec2D(2, 0), body.velocity)
+	}
+
+
+
+	@Test
+	def addBodyApplyForceStepTwiceCheckVelocity {
+		val world = new World
+		val body = new Body
+		body.mass = 5
+		body.applyForce(Vec2D(5, 0))
+		world.add(body)
+		world.step(2.0)
+		world.step(2.0)
+		assertEquals(Vec2D(2, 0), body.velocity)
+	}
+}
