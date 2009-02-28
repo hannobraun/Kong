4943ee63fa28986ddcff0b21b6a1db45a1c0b47e
Hanno Braun <hanno@habraun.net>
2009-02-28 12:29:48 +0100

    Physics: Code formatting correction.

--- src/net/habraun/kong/physics/Collision.scala
+++ src/net/habraun/kong/physics/Collision.scala
@@ -24,5 +24,5 @@ case class Collision(b1: Body, b2: Body, normal1: Vec2D, normal2: Vec2D, impactP
 	if (normal1 != normal2.inverse)
 		throw new IllegalArgumentException("Both collision normals must be inverse to each other.")
 	if (normal1 * normal1 != 1)
-	throw new IllegalArgumentException("Normals must be unit vectors.")
+		throw new IllegalArgumentException("Normals must be unit vectors.")
 }
